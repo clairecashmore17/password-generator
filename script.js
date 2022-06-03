b1c7a91c8b78fb6fac1aecfbdca3741f7afc7383
Claire Cashmore <clairecashmore2017@gmail.com>
2022-06-03 17:51:26 -0400

    added criteria case recognition

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 function generatePassword(){
     var password = {
         length : 0,
-        lower : 0,
-        upper : 0,
-        num : 0,
-        special : 0
+        lower : false,
+        upper : false,
+        num : false,
+        special : false
 
     };
     /******FUNCTIONS*********/
 
     //Select password criteria (must choose one)
-    var userLength = function(){
+    function userLength(){
         var length = window.prompt("How long would you like your password to be? Must be 8-128 characters: ");
         length = parseInt(length);
         if(length < 8 || length > 128){
@@ -23,45 +23,109 @@ function generatePassword(){
         notChosenCriteria = false;
         return length;
     }
-    /******End Function*********/
 
-    // Alert user we are making a password
-    window.alert("Let's generate you a password!");
 
-    // As long as a user has chosen at least one criteria.
+    function chooseCriteria(){
+
+    // Function to choose anothe criteria 
+    function anotherCrit() {
+        var again = window.confirm("Would you like to pick another criteria?");
+        //If user disagrees to choose another criteria
+        if(!again) {
+            chooseAnother = false;
+        }
+        return chooseAnother;
+    }
+            // As long as a user has chosen at least one criteria.
     var notChosenCriteria = true;
     while(notChosenCriteria){
-        //Length between 8 - 128
+        // Ask for Length between 8 - 128
         password.length = userLength();
         console.log(password.length);        
         
         // Character Cases
-        // var chooseAnother = true;
-        // while(chooseAnother){
-        //   var userCharacter = window.prompt("Would you like to have LOWERCASE(1), UPPERCASE(2) , NUMBERS(3), OR SPECIALCHARS(4)?: ");
-        //  userCharacter = parseInt(userCharacter);
-        //   // Lowercase
-        //   switch(userCharacter){
-        //     case 1:
-        //       console.log()
-        //       break;
-        //   //Uppercase
-        //     case 2:
-        //       break;
-        //   // Numeric
-        //     case 3:
-        //       break;
-        //   //Special
-        //   case 4:
-        //     break;
-        //   default:
-        //     console.log("User did not give a proper answer");
-        //     //add recursive function here.
-        //     break;
-        //   }
-        // }
+        var chooseAnother = true;
+        while(chooseAnother){
+          var userInput = window.prompt("Would you like to have LOWERCASE(1), UPPERCASE(2) , NUMBERS(3), OR SPECIALCHARS(4)? Or to QUIT(5): ");
+         userInput = parseInt(userInput);
+          // Lowercase
+          switch(userInput){
+            case 1:
+              console.log("Chose 1");
+              if( password.lower === true){
+                  window.alert("You already asked for lower chars");
+                  break;
+              }
+              password.lower = true;
+              notChosenCriteria = false;
+              console.log(password.lower);
+              chooseAnother = anotherCrit();
+              break;
+          //Uppercase
+            case 2:
+                console.log("Chose 2");
+                if( password.upper === true){
+                    window.alert("You already asked for upper chars");
+                    break;
+                }
+                password.upper = true;
+                notChosenCriteria = false;
+                console.log(password.upper);
+                chooseAnother = anotherCrit();
+              break;
+          // Numeric
+            case 3:
+                console.log("Chose 3");
+                if( password.num === true){
+                    window.alert("You already asked for numbers");
+                    break;
+                }
+                password.num = true;
+                notChosenCriteria = false;
+                console.log(password.num);
+                chooseAnother = anotherCrit();
+              break;
+          //Special
+          case 4:
+            if( password.special === true){
+                window.alert("You already asked for special chars");
+                break;
+            }
+            
+            console.log("Chose 4");
+            password.special = true;
+            notChosenCriteria = false;
+            console.log(password.special);
+            chooseAnother = anotherCrit();
+            break;
+            case 5:
+                if(notChosenCriteria === false){
+                    chooseAnother = false;
+                    console.log("User chose to quit");
+                    break;
+                }
+                else{
+                    window.alert("You must choose at least one criteria!");
+                    break;
+                }
+          default:
+            window.alert("Not a valid answer, try again.");
+            console.log("User did not give a proper answer");
+            
+            break;
+          }
+        }
   
     }
+        
+    }
+
+    /******End Functions*********/
+
+    // Alert user we are making a password
+    window.alert("Let's generate you a password!");
+    chooseCriteria();
+
     return password;
   }
   
